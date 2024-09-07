caa7ca2aef5dc4afa609f8e4dcbcc68ab6ae2f57
Nihat04 <aliev.nihat4@gmail.com>
2024-09-07 21:34:32 +0500

    Cleared Repo

--- src/components/PlayersPickList/PlayersPickList.jsx
+++
@@ -1,9 +0,0 @@
-import { useSelector } from "react-redux";
-
-const PlayersPickList = () => {
-    const players = useSelector((state) => state.players)
-    console.log(players)
-    return <ul></ul>;
-};
-
-export default PlayersPickList;
